384855db961f7e388be3b3cc17bab2236cf35a4e
Sam <sam.brown@madetech.com>
2023-01-11 11:59:15 +0000

    script outputs answer to day 1 part 2

--- Day-1/Ouput.cs
+++ Day-1/Ouput.cs
@@ -1,6 +1,6 @@
-using Day_1;
+using AdventOfCode;
 
-namespace AdventOfCode
+namespace Day_1
 {
     public class Output
     {
@@ -8,10 +8,12 @@ namespace AdventOfCode
             File.ReadAllText("/Users/sam.brown/Advent-of-code-2022/Day-1/input.txt");
         private static readonly string[] FormattedInput = InputConverter.Split(Input,"\n\n");
         private static readonly int MostCalories = Calories.HighestCalorie(FormattedInput);
-        
+        private static readonly List<int> TotalCalorieArray = Calories.TotalCalorieArray(FormattedInput);
+        private static readonly int TopThree = Calories.SumHighestXCalories(TotalCalorieArray, 3);
         static void Main(string[] args)
         {
             Console.WriteLine(MostCalories);
+            Console.WriteLine(TopThree);
         }
     }
 }
