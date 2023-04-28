831a4e28956f48cf68b094ae78841f07eb4e83e7
Sam <sam.brown@madetech.com>
2023-04-28 12:18:00 +0100

    initial section formatter

--- Day-4-tests/UnitTest1.cs
+++ Day-4-tests/UnitTest1.cs
@@ -8,6 +8,12 @@ public class Tests
     public void ElfFormatter()
     {
         var elves = "1-2,2-3";
-        Assert.That(Cleanup.ElfFormatter(elves),Is.EqualTo(new int[,]{{1,2},{2,3}}));
+        Assert.That(Cleanup.ElfFormatter(elves),Is.EqualTo(new int[][] { new int[] {1,2},new int[] {2,3}}));
+    }
+    [Test]
+    public void ElfFormatter2()
+    {
+        var elves = "1-5,2-5";
+        Assert.That(Cleanup.ElfFormatter(elves),Is.EqualTo(new int[][]{new int[] {1,5},new int[] {2,5}}));
     }
 }
--- Day-4/Program.cs
+++ Day-4/Program.cs
@@ -1,11 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Immutable;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Day_4;
 
 public class Cleanup
 {
-    public static int[,] ElfFormatter(string elves)
+    public static int[][] ElfFormatter(string elves)
     {
-        return new int[,] { { 1, 2 }, { 2, 3 } };
+        var sectionArray = elves.Split(",");
+        var sectionMArray = sectionArray.Select(ElfSectionFormatter).ToArray();
+        return sectionMArray;
+    }
+
+    private static int[] ElfSectionFormatter(string elfSection)
+    {
+        var sectionArray = elfSection.Split("-");
+        var intSectionArray = sectionArray.Select(section => int.Parse(section)).ToArray();
+        return intSectionArray;
+
     }
 }
