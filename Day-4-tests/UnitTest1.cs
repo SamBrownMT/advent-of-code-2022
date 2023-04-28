0d041e72fb01c6ede3995dd7ab8fbffb77f616fd
Sam <sam.brown@madetech.com>
2023-04-28 15:15:58 +0100

    add function to check for fully containedness

--- Day-4-tests/UnitTest1.cs
+++ Day-4-tests/UnitTest1.cs
@@ -16,4 +16,28 @@ public class Tests
         var elves = "1-5,2-5";
         Assert.That(Cleanup.ElfFormatter(elves),Is.EqualTo(new int[][]{new int[] {1,5},new int[] {2,5}}));
     }
+    [Test]
+    public void IsFullyContained()
+    {
+        var elves = new int[][] {new int[] {2,3},new int[]{1,4}};
+        Assert.That(Cleanup.IsFullyContained(elves),Is.EqualTo(true));
+    }
+    [Test]
+    public void IsFullyContained2()
+    {
+        var elves = new int[][] {new int[] {2,5},new int[]{1,4}};
+        Assert.That(Cleanup.IsFullyContained(elves),Is.EqualTo(false));
+    }
+    [Test]
+    public void IsFullyContained3()
+    {
+        var elves = new int[][] {new int[] {2,5},new int[]{2,4}};
+        Assert.That(Cleanup.IsFullyContained(elves),Is.EqualTo(true));
+    }
+    [Test]
+    public void IsFullyContained4()
+    {
+        var elves = new int[][] {new int[] {2,5},new int[]{4,5}};
+        Assert.That(Cleanup.IsFullyContained(elves),Is.EqualTo(true));
+    }
 }
--- Day-4/Program.cs
+++ Day-4/Program.cs
@@ -17,8 +17,23 @@ public class Cleanup
     private static int[] ElfSectionFormatter(string elfSection)
     {
         var sectionArray = elfSection.Split("-");
-        var intSectionArray = sectionArray.Select(section => int.Parse(section)).ToArray();
+        var intSectionArray = sectionArray.Select(int.Parse).ToArray();
         return intSectionArray;
 
+    }
+
+    public static bool IsFullyContained(int[][] elves)
+    {
+        if (elves[0][0] <= elves[1][0] && elves[0][1] >= elves[1][1])
+        {
+            return true;
+        }
+        
+        if (elves[0][0] >= elves[1][0] && elves[0][1] <= elves[1][1])
+        {
+            return true;
+        }
+        
+        return false;
     }
 }
